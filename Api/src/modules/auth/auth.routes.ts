50f96fede0e44f4a8fe219649040817fd393c73d
Sion Arancibia <sionarancibia@gmail.com>
2025-09-13 20:12:01 -0300

    TDI-12: Configurar rate-limiting para evitar ataques de fuerza bruta.

--- Api/src/modules/auth/auth.routes.ts
+++ Api/src/modules/auth/auth.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { register, login, me, refresh, logout, recoverPassword, resetPassword} from './auth.controller.js';
 import { authMiddleware, validateRecoverPassword, validateResetPassword} from '../../middlewares/auth.middleware.js';
-
+import { authLimiter } from '../../middlewares/ratelimit.middleware.js';
 const router = Router();
 
 router.post('/register', register);
-router.post('/login', login);
+router.post('/login', authLimiter, login);
 router.post('/refresh', refresh);
 router.post('/logout', logout);
 
 router.get('/me', authMiddleware, me);
-router.post("/recover-password", validateRecoverPassword, recoverPassword);
-router.post("/reset-password", validateResetPassword, resetPassword);
+router.post("/recover-password", authLimiter, validateRecoverPassword, recoverPassword);
+router.post("/reset-password", authLimiter, validateResetPassword, resetPassword);
 
 export default router;
