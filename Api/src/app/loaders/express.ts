d80afe4cb5172c088c897b76a1b3859642b94c0b
Sion Arancibia <sionarancibia@gmail.com>
2025-09-13 20:33:05 -0300

    TDI-10: Configurar middlewares de seguridad básicos (helmet, cors) y se eliminan archivos innecesarios

--- Api/src/app/loaders/express.ts
+++ Api/src/app/loaders/express.ts
@@ -2,11 +2,23 @@ import express from 'express';
 import routes from '../routes.js';
 import cors from 'cors';
 import { errorHandler } from '../../middlewares/error.middleware.js';
-import { requestLogger } from "../../middlewares/logger.js";
+import { requestLogger } from "../../middlewares/logger.middleware.js";
+import helmet from 'helmet';
 
 export const createApp = () => {
   const app = express();
-  app.use(cors());
+  app.use(helmet()) // Helmet Configura cabeceras HTTP seguras
+  app.use(cors()); // CORS Controla qué dominios pueden acceder a la API
+
+  // app.use(
+  //   cors({
+  //     origin: ["http://localhost:3000"], // Solo permite requests desde este dominio, configurar los de 2do cuando hagan deploy
+  //     methods: ["GET", "POST", "PUT", "DELETE"],
+  //     allowedHeaders: ["Content-Type", "Authorization"],
+  //     credentials: true,
+  //   })
+  // );
+  
   app.use(express.json());
   app.use(requestLogger); // logger
   app.use('/api', routes);
